5b019d9728d83f34bc8538da7fe7a609d0593001
qiying_wu <qiying_wu@ohsa.com.cn>
2023-11-27 10:55:43 +0800

    测试immer git 提交记录

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,9 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Table, Card, Button } from "antd";
 import { produce } from "immer";
-import { useImmer,useImmerReducer } from "use-immer"
+import { useImmer, useImmerReducer } from "use-immer"
 
 const initialState = {
   users: [
@@ -33,14 +33,14 @@ const HomePage = () => {
   }
   const [dataMap, setDataMap] = useState(data)
   const [person, updatePerson] = useImmer(data)
-  
+
   const [state, dispatch] = useImmerReducer(reducer, initialState);
   // 使用useImmerReducer创建的Reducer函数中，我们不需要再手动处理状态的更新，
   // 因为useImmerReducer已经自动使用immer来处理了状态的更新。这样可以让我们更方便地使用可变的操作来修改原有对象或数组中的某一项
-  
+
   const updateUser = (payload) => {
-      dispatch({ type: 'updateUser', payload });
-    };
+    dispatch({ type: 'updateUser', payload });
+  };
   // 在updateUser函数中，我们通过调用dispatch函数并传入updateUser类型的action来更新一个用户的信息。
   // 在Reducer函数中，我们使用find方法找到指定ID的用户对象，并直接修改其name和age属性。
 
@@ -50,14 +50,14 @@ const HomePage = () => {
     { title: 'Address', dataIndex: 'address', key: 'address', fixed: 'left' },
     { title: 'Action', key: 'operation', fixed: 'right', render: () => <a>action</a> },
   ];
- 
+
 
   function updateName(name) {
     updatePerson(draft => {
       draft[0].name = name;
     });
-}
- 
+  }
+
   const modifyDefaultData = () => {
     dataMap[0].name = 'Jim'
     console.log(dataMap);
@@ -70,23 +70,23 @@ const HomePage = () => {
     console.log(person);
     console.log(state.users);
     // 不会执行
-  }, [dataMap,person,state.users])
+  }, [dataMap, person, state.users])
 
   return (
     <PageContainer ghost>
       <Card extra={<>
         <Button onClick={modifyDefaultData}>修改默认数据</Button>
-        <Button onClick={()=>updateName('lili')}>使用immer修改默认数据</Button>
-        <Button onClick={()=>updateUser({ id: 2, name: 'Jane Doe', age: 35 })}>使用immer-useImmerReducer修改默认数据</Button>
+        <Button onClick={() => updateName('lili')}>使用immer修改默认数据</Button>
+        <Button onClick={() => updateUser({ id: 2, name: 'Jane Doe', age: 35 })}>使用immer-useImmerReducer修改默认数据</Button>
       </>}>
         <Table columns={columns} dataSource={dataMap} />
         <ul>
-        {state.users.map(user => (
-          <li key={user.id}>
-            {user.name} - {user.age}
-          </li>
-        ))}
-      </ul>
+          {state.users.map(user => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
       </Card>
     </PageContainer>
   );
