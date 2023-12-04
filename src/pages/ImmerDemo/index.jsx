9ee3708187acc3c4330672104bdc12dceec35eb3
qiying_wu <qiying_wu@ohsa.com.cn>
2023-12-04 09:25:21 +0800

    修改代码格式

--- src/pages/ImmerDemo/index.jsx
+++ src/pages/ImmerDemo/index.jsx
@@ -1,93 +1,90 @@
 import { PageContainer } from '@ant-design/pro-components';
-import { Button, Divider, Drawer, message, Card } from 'antd';
-import { useCallback, useRef, useState } from 'react';
+import { Button, Card } from 'antd';
 
 const ImmerDemo = () => {
+    // 首先实现一个简单的共享状态的拷贝功能
 
-  // 首先实现一个简单的共享状态的拷贝功能 
+    // let cope = { ...state }
+    // cope.email = '111@qq.com'
+    // console.log(copo.name === state.name) // true 这里并没有去改变初始state中的值
 
+    // step2
+    // 实现一个简单的immer
 
-  // let cope = { ...state }
-  // cope.email = '111@qq.com'
-  // console.log(copo.name === state.name) // true 这里并没有去改变初始state中的值
-
-  // step2
-  // 实现一个简单的immer
-
-  const state = {
-    name: 'lily', email: "mina@gmail.com", hobbies: {
-      read: '1'
-    }
-  }
-
-  // function immer(state, thunk) {
-  //   const handler = {
-  //     get(target, prop) {
-  //       return target[prop]; // 直接返回属性值
-  //     },
-
-
-  //     set(target, prop, value) {
-  //       const copy = { ...target };
-  //       // 浅拷贝只复制了原始数据对象的属性 而没有复制属性值所引用的对象 
-  //       // 对于属性值是对象的情况copy的对象和原始数据对象会共享同一个对象
-  //       copy[prop] = value; // 给拷贝对象赋值
-  //       thunk(copy); // 调用 thunk 函数÷
-  //       return true; // 必须返回 true 表示修改成功
-  //     }
-  //   };
-
-  //   const proxy = new Proxy(state, handler);
-  //   thunk(proxy);
-  //   return proxy;
-  // }
-
-  // const newState = immer(state, draft => {
-  //   draft.name = 'Bob';
-  //   draft.email = 'qqaa';
-  //   draft.hobbies.read = '3';
-  // });
-
-  function immer(state, thunk) {
-    const handler = {
-      get(target, prop) {
-        return target[prop]; // 直接返回属性值
-      },
-      set(target, prop, value) {
-        let copy = target;
-        // 浅拷贝只复制了原始数据对象的属性 而没有复制属性值所引用的对象 
-        // 对于属性值是对象的情况copy的对象和原始数据对象会共享同一个对象
-        copy[prop] = value; // 给拷贝对象赋值
-        thunk(copy); // 调用 thunk 函数，并传递修改后的对象
-        return true; // 返回 true 表示修改成功
-      }
+    const state = {
+        name: 'lily',
+        email: 'mina@gmail.com',
+        hobbies: {
+            read: '1',
+        },
     };
 
-    const proxy = new Proxy(state, handler);
-    thunk(proxy);
-    return proxy; // 返回代理对象
-  }
+    function immer(state, callback) {
+        const handler = {
+            get(target, prop) {
+                let value = target[prop]; // 草稿
+                if (typeof value === 'object' && value !== null) {
+                    return new Proxy(value, handler); // 对嵌套对象进行代理
+                }
+                return value;
+            },
 
-  const newState = immer(state, draft => {
-    draft.name = 'Bob';
-    draft.email = 'qqaa';
-    draft.hobbies = { ...draft.hobbies, read: '3' }; // 修改嵌套对象的属性
-  });
+            // set(target, prop, value) {
+            //   let copy = Array.isArray(target) ? [...target] : { ...target };
+            //   console.log(copy,prop,value);
+            //   copy[prop] = value;
+            //   callback(copy);
+            //   return true;
+            // }
 
-  console.log(JSON.stringify(newState))
+            // 错误示例
+            set(target, prop, value) {
+                let copy = target;
+                // 浅拷贝只复制了原始数据对象的属性 而没有复制属性值所引用的对象
+                // 对于属性值是对象的情况copy的对象和原始数据对象会共享同一个对象
+                copy[prop] = value; // 给拷贝对象赋值
+                callback(copy); // 调用 c'a'l'l'ba'c'k 函数，并传递修改后的对象
+                return true; // 返回 true 表示修改成功
+            },
+        };
 
-  return (
-    <PageContainer
-      header={{
-        title: 'Immerdemo',
-      }}
-    >
-      <Card title='immer-demo' exter={<>
-        <Button onClick={() => handlerChange('bd')}>changeValue</Button>
-      </>} />
+        const proxy = new Proxy(state, handler);
+        callback(proxy);
+        return proxy; // 返回代理对象
+    }
 
-    </PageContainer>
-  );
+    const newState = immer(state, (draft) => {
+        draft.name = 'Bob';
+        draft.email = 'qqaa';
+        if (!draft.hobbies) {
+            // 如果 hobbies 对象不存在，则进行初始化
+            draft.hobbies = {};
+        }
+        draft.hobbies.read = '3';
+        // 直接写draft.hobbies.read = '3';可能导致找不到hobbies
+        // draft.hobbies = { ...draft.hobbies, read: '3' };  修改嵌套对象的性
+    });
+
+    console.log(newState);
+    console.log(JSON.stringify(newState));
+    // 这里返回的还是个proxy对象 查看的话可以可以使用JSON.stringify打印查看
+
+    return (
+        <PageContainer
+            header={{
+                title: 'Immerdemo',
+            }}
+        >
+            <Card
+                title="immer-demo"
+                exter={
+                    <>
+                        <Button onClick={() => handlerChange('bd')}>changeValue</Button>
+                    </>
+                }
+            />
+        </PageContainer>
+    );
 };
 
 export default ImmerDemo;
--- src/pages/Produce/index.jsx
+++ src/pages/Produce/index.jsx
@@ -1,82 +1,85 @@
-import { PageContainer, } from '@ant-design/pro-components';
-import { useState, useCallback } from "react";
+import { PageContainer } from '@ant-design/pro-components';
 import { useAccess } from '@umijs/max';
 import { Button, Card, Table } from 'antd';
-import { produce } from "immer"
+import { produce } from 'immer';
+import { useCallback, useState } from 'react';
 
 export default function AccessPage() {
-  const access = useAccess();
-  const [todoList, setTodolist] = useState([
-    { name: "lily", age: 12 },
-    { name: 'judy', age: 13 }
-  ])
+    const access = useAccess();
+    const [todoList, setTodolist] = useState([
+        { name: 'lily', age: 12 },
+        { name: 'judy', age: 13 },
+    ]);
 
-  const columns = [
-    { title: 'Full Name', dataIndex: 'name', key: 'name', fixed: 'left' },
-    { title: 'Age', dataIndex: 'age', key: 'age', fixed: 'left' },
-  ]
+    const columns = [
+        { title: 'Full Name', dataIndex: 'name', key: 'name', fixed: 'left' },
+        { title: 'Age', dataIndex: 'age', key: 'age', fixed: 'left' },
+    ];
 
-  // 直接使用deepCopy和使用produce有什么区别
+    // 直接使用deepCopy和使用produce有什么区别
 
-  // 增
-  const handleAdd = useCallback((obj) => {
-    setTodolist(
-      produce((draft) => {
-        draft.push(obj)
-      })
-    )
-  })
+    // 增
+    const handleAdd = useCallback((obj) => {
+        setTodolist(
+            produce((draft) => {
+                draft.push(obj);
+            }),
+        );
+    });
 
-  // 删
-  const handleRemove = useCallback((val) => {
-    setTodolist(
-      produce((draft) => {
-        draft.splice(draft.findIndex(index => index.name === val), 1)
-      })
-    )
-  })
+    // 删
+    const handleRemove = useCallback((val) => {
+        setTodolist(
+            produce((draft) => {
+                draft.splice(
+                    draft.findIndex((index) => index.name === val),
+                    1,
+                );
+            }),
+        );
+    });
 
-  // 改
-  const handleChange = useCallback((val1, val2, val3) => {
-    setTodolist(
-      produce((draft) => {
-        let todu = draft.findIndex(index => index.name === val1)
-        draft[todu][val2] = val3
-      })
-    )
-  })
+    // 改
+    const handleChange = useCallback((val1, val2, val3) => {
+        setTodolist(
+            produce((draft) => {
+                let todu = draft.findIndex((index) => index.name === val1);
+                draft[todu][val2] = val3;
+            }),
+        );
+    });
 
-  // 查
-  const handleSearch = useCallback((val) => {
-    setTodolist(
-      produce((draft) => {
-        // 这里filter返回的是一个新数组 则需要return   pusu splice 等函数是直接修改原始数组的方法，它们会改变原始数组并返回修改后的数组长度，而不是返回修改后的数组本身
-        return draft.filter(index => index.name === val)
-      })
-    )
-  })
+    // 查
+    const handleSearch = useCallback((val) => {
+        setTodolist(
+            produce((draft) => {
+                // 这里filter返回的是一个新数组 则需要return   pusu splice 等函数是直接修改原始数组的方法，它们会改变原始数组并返回修改后的数组长度，而不是返回修改后的数组本身
+                return draft.filter((index) => index.name === val);
+            }),
+        );
+    });
 
-  return (
-    <PageContainer
-      ghost
-      header={{
-        title: 'produce',
-      }}
-    >
-      <Card
-        extra={
-          <>
-            <Button onClick={() => handleAdd({ name: 'yanguang', age: 21 })}>add</Button>
-            <Button onClick={() => handleRemove('judy')}>remove</Button>
-            <Button onClick={() => handleChange('lily', "age", 88)}>edit</Button>
-            <Button onClick={() => handleSearch('judy')}>search</Button>
-          </>
-        }
-      >
-        <Table columns={columns} dataSource={todoList} />
-      </Card>
-    </PageContainer>
-  );
-};
+    return (
+        <PageContainer
+            ghost
+            header={{
+                title: 'produce',
+            }}
+        >
+            <Card
+                extra={
+                    <>
+                        <Button onClick={() => handleAdd({ name: 'yanguang', age: 21 })}>add</Button>
+                        <Button onClick={() => handleRemove('judy')}>remove</Button>
+                        <Button onClick={() => handleChange('lily', 'age', 88)}>edit</Button>
+                        <Button onClick={() => handleSearch('judy')}>search</Button>
+                    </>
+                }
+            >
+                <Table columns={columns} dataSource={todoList} />
+            </Card>
+        </PageContainer>
+    );
+}
 
 AccessPage;
