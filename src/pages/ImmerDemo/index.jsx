a6417bd44887c3818af069c066be4c161e6669fd
qiying_wu <qiying_wu@ohsa.com.cn>
2023-12-19 16:58:06 +0800

    提交 redux for immer ep

--- src/pages/ImmerDemo/index.jsx
+++ src/pages/ImmerDemo/index.jsx
@@ -45,65 +45,104 @@ const ImmerDemo = () => {
     // version2
     // ======================================================================================================
 
-    function produce(state, callback) {
-        const proxy = createProxy(state, callback);
-        callback(proxy);
-        return proxy._value;
+    function produce(baseState, producer) {
+        // 深度克隆初始状态
+        const clonedState = deepClone(baseState);
+        // 创建 Proxy 对象
+        const proxy = createProxy(clonedState);
+        // 执行 producer 函数，其中对 proxy 的任何修改都会被捕获
+        producer(proxy);
+        // 获取最终的修改结果
+        const result = getFinalResult(clonedState);
+        return result;
     }
 
-    function createProxy(target, callback) {
+    // 创建 Proxy 对象的函数
+    function createProxy(baseState) {
+        // 用于存储修改的路径和对应的值
+        const patches = [];
+        // 使用 Proxy 拦截对象的操作
         const handler = {
             get(target, prop) {
-                const value = target[prop];
-                if (typeof value === 'object' && value !== null) {
-                    return createProxy(value, callback);
+                // 如果属性是一个对象，递归创建 Proxy
+                if (typeof target[prop] === 'object' && target[prop] !== null) {
+                    return createProxy(target[prop], (path) => `${prop}.${path}`);
                 }
-                return value;
+                return target[prop];
             },
             set(target, prop, value) {
-                const copy = deepCopy(target);
-                copy[prop] = value;
-                callback(copy);
+                // 记录修改的路径和对应的值
+                patches.push({ path: prop, value });
+                // 应用修改到克隆对象
+                target[prop] = value;
                 return true;
             },
         };
-        const proxy = new Proxy({ _value: target }, handler);
+        // 创建 Proxy 对象
+        const proxy = new Proxy(baseState, handler);
+        // 递归创建 Proxy 的函数
+        function createProxy(target, pathFn) {
+            return new Proxy(target, {
+                get(subTarget, prop) {
+                    if (typeof subTarget[prop] === 'object' && subTarget[prop] !== null) {
+                        return createProxy(subTarget[prop], (path) => `${pathFn(prop)}.${path}`);
+                    }
+                    return subTarget[prop];
+                },
+                set(subTarget, prop, value) {
+                    patches.push({ path: pathFn(prop), value });
+                    subTarget[prop] = value;
+                    return true;
+                },
+            });
+        }
+        // 在 Proxy 上附加方法，用于获取最终的修改结果
+        proxy.getPatches = () => patches;
         return proxy;
     }
 
-    function deepCopy(obj) {
-        if (typeof obj !== 'object' || obj === null) {
-            return obj;
-        }
-        const copy = Array.isArray(obj) ? [] : {};
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                copy[key] = deepCopy(obj[key]);
-            }
-        }
-        return copy;
+    // 获取最终的修改结果
+    function getFinalResult(clonedState) {
+        const patches = clonedState.getPatches();
+        // 应用所有的修改到原始对象，得到最终的结果
+        const result = applyPatches(clonedState, patches);
+        return result;
     }
 
-    // 使用 produce 函数进行状态修改
-    const state = {
-        name: 'Alice',
-        age: 25,
-        hobbies: ['reading', 'painting'],
-    };
+    // 应用修改到原始对象的函数
+    function applyPatches(baseState, patches) {
+        // 克隆原始对象
+        const nextState = deepClone(baseState);
+        // 遍历所有的修改，将其应用到新对象上
+        patches.forEach((patch) => {
+            const path = patch.path.split('.');
+            let target = nextState;
+            // 寻找修改的目标对象
+            for (let i = 0; i < path.length - 1; i++) {
+                target = target[path[i]];
+            }
+            // 应用修改
+            target[path[path.length - 1]] = patch.value;
+        });
 
-    const newState = produce(state, (draft) => {
-        draft.name = 'Bob';
-        draft.age = 30;
-        if (!draft.hobbies) {
-            draft.hobbies = [];
-        } else {
-            draft.hobbies.push('swimming');
-        }
+        return nextState;
+    }
+
+    // 深度克隆对象的函数
+    function deepClone(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+    // 示例
+    const state = { a: 1, b: { c: 2 } };
+    const nextState = produce(state, (draft) => {
+        draft.a = 42;
+        draft.b.c = 99;
     });
 
-    console.log(state); // { name: 'Alice', age: 25, hobbies: [ 'reading', 'painting' ] }
-    console.log(newState); // { name: 'Bob', age: 30, hobbies: [ 'reading', 'painting', 'swimming' ] }
-    console.log(JSON.stringify(newState));
+    console.log(state); // 输出: { a: 1, b: { c: 2 } }
+    console.log(nextState); // 输出: { a: 42, b: { c: 99 } }
+
     return (
         <PageContainer
             header={{
--- src/pages/ImmerForRedux/index.jsx
+++ src/pages/ImmerForRedux/index.jsx
@@ -1,104 +1,69 @@
-import React, { useState } from 'react';
-import { Input, Button } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  createUser,
-  editUser,
-  saveUser,
-  resetUser,
-  deleteUser,
-} from './actionType';
+import { Button } from 'antd';
+import { produce } from 'immer';
+import { createStore } from 'redux';
 
 const UserForm = () => {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  const [userInfo, setUserInfo] = useState({
-    name: '',
-    age: '',
-    address: '',
-  });
+    // 初始状态
+    const initialState = {
+        todos: [
+            { id: 1, text: 'Buy groceries', completed: false },
+            { id: 2, text: 'Write code', completed: true },
+        ],
+    };
 
-  const handleCreateUser = () => {
-    dispatch(createUser(userInfo));
-  };
+    // Reducer 函数，使用 Immer 处理状态变更
+    const todosReducer = (state = initialState, action) => {
+        return produce(state, (draft) => {
+            switch (action.type) {
+                case 'ADD_TODO':
+                    // 增加 todo
+                    draft.todos.push({ id: action.id, text: action.text, completed: false });
+                    break;
+                case 'TOGGLE_TODO':
+                    // 切换 todo 的完成状态
+                    const toggledTodo = draft.todos.find((todo) => todo.id === action.id);
+                    if (toggledTodo) {
+                        toggledTodo.completed = !toggledTodo.completed;
+                    }
+                    break;
+                // 删除
+                case 'DELETE_TODO':
+                    draft.todos = draft.todos.splice(draft.todos.findIndex((todo) => todo.id === action.id));
+                    break;
+                // 编辑
+                case 'EDIT_TODO':
+                    draft.todos[draft.todos.findIndex((todo) => todo.id === action.id)]['text'] = action.text;
+                    break;
+                // 查找
+                case 'SEARCH_TODO':
+                    draft.todos = draft.todos.filter((todo) => todo.id === action.id);
+                    break;
+                case 'RESET_TODOS':
+                    // 重置 todos 到初始状态
+                    draft.todos = initialState.todos;
+                    break;
+                default:
+                    break;
+            }
+        });
+    };
 
-  const handleEditUser = () => {
-    dispatch(editUser(userInfo));
-  };
+    // 创建 Redux store
+    const store = createStore(todosReducer);
+    // 订阅 state 变化
+    store.subscribe(() => {
+        console.log('Current State:', store.getState());
+    });
 
-  const handleSaveUser = () => {
-    dispatch(saveUser());
-  };
-
-  const handleResetUser = () => {
-    dispatch(resetUser());
-  };
-
-  const handleDeleteUser = () => {
-    dispatch(deleteUser());
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserInfo((prevUserInfo) => ({
-      ...prevUserInfo,
-      [name]: value,
-    }));
-  };
-
-  return (
-    <div>
-      {user ? (
-        <>
-          <h2>Edit User</h2>
-          <Input
-            type="text"
-            name="name"
-            value={userInfo.name}
-            onChange={handleChange}
-          />
-          <Input
-            type="text"
-            name="age"
-            value={userInfo.age}
-            onChange={handleChange}
-          />
-          <Input
-            type="text"
-            name="address"
-            value={userInfo.address}
-            onChange={handleChange}
-          />
-          <Button onClick={handleSaveUser}>Save</Button>
-          <Button onClick={handleResetUser}>Reset</Button>
-          <Button onClick={handleDeleteUser}>Delete</Button>
-        </>
-      ) : (
-        <>
-          <h2>Create User</h2>
-          <Input
-            type="text"
-            name="name"
-            value={userInfo.name}
-            onChange={handleChange}
-          />
-          <Input
-            type="text"
-            name="age"
-            value={userInfo.age}
-            onChange={handleChange}
-          />
-          <Input
-            type="text"
-            name="address"
-            value={userInfo.address}
-            onChange={handleChange}
-          />
-          <Button onClick={handleCreateUser}>Create</Button>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <Button onClick={() => store.dispatch({ type: 'ADD_TODO', id: 3, text: 'Learn Redux' })}>Add</Button>
+            <Button onClick={() => store.dispatch({ type: 'DELETE_TODO', id: 2 })}>Del</Button>
+            <Button onClick={() => store.dispatch({ type: 'EDIT_TODO', id: 1, text: 'Hello,world' })}>Edit</Button>
+            <Button onClick={() => store.dispatch({ type: 'SEARCH_TODO', id: 1 })}>Search</Button>
+            <Button onClick={() => store.dispatch({ type: 'RESET_TODOS' })}> Reset</Button>
+        </div>
+    );
 };
 
 export default UserForm;
